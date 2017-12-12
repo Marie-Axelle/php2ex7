ce14b6432ec7234e2971f39d3c9a73c47373d891
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-12 14:59:48 +0100

    ex 7

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php</title>
+  </head>
+  <body>
+    <?php
+    $maVariable = true;
+
+    if ($maVariable == false) {
+    echo 'c\'est pas bon !!!';
+    }
+    else {
+      echo 'c\'est ok !!';
+    }
+     ?>
+  </body>
+</html>
